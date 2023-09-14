f03971bd1991a642609d0795ae31e0457acd918d
John Corrigan <jcorrigan322@gmail.com>
2023-09-13 18:28:20 -0700

    live swagger page? can hit endpoints in live ui?

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,11 +1,20 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Notes api",
+        Version = "v1"
+    });
+});
 
 
 var app = builder.Build();
@@ -13,8 +22,19 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Notes api V1");
+    });
 }
+else 
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+    });
+} 
 
 app.UseHttpsRedirection();
 
@@ -22,9 +42,10 @@ app.UseAuthorization();
 
 app.UseCors(options =>
 {
+    options.WithOrigins("http://localhost:5173", "https://johnnotesapi.azurewebsites.net", "https://notes.johncorrigan.dev");
     //options.AllowAnyHeader();
     //options.AllowAnyMethod();
-    options.AllowAnyOrigin();
+    //options.AllowAnyOrigin();
 });
 
 app.MapControllers();
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,10 +3,36 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import { ref } from 'vue'
 
+interface Comment {
+  user: User;
+  body: string;
+  postedDate: string;
+}
+interface Author {
+  username: string;
+  id: number;
+}
+
+interface User {
+  username: string;
+  id: number;
+}
+
+interface Post {
+  body: string;
+  postedDate: string;
+  comments: Comment[];
+  author: Author;
+  likes: number;
+  id: number;
+}
+
 const date = ref("");
 const temp = ref("");
 const tempF = ref("");
 const summary = ref("");
+
+const top3 = ref<Post[]>();
 
 // const getWeather = async () => {
   // const weather = await fetch("https://localhost:7010/WeatherForecast")
@@ -30,15 +56,42 @@ const getWeather: any = async () => {
     });
 }
 
+const getTop3: any = async () => {
+  fetch("https://johnnotesapi.azurewebsites.net/api/Post/top3")
+  // fetch("https://localhost:7010/api/Post/top3")
+    .then(response => {
+      // console.log(response.json());
+      if(!response.ok) {
+        throw new Error("HTTP error " + response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(JSON.stringify(data));
+      top3.value = JSON.parse(JSON.stringify(data)) as Post[];
+      console.log(top3.value)
+    });
+}
+
 </script>
 
 <template>
      <div>
-        <button @click="getWeather">Get weather</button>
+        <button @click="getWeather">Get weather local</button>
         <p>{{date}}</p>
         <p>{{temp}}</p>
         <p>{{tempF}}</p>
         <p>{{summary}}</p>
+      </div>
+
+      <div>
+        <button @click="getTop3">Get top3</button>
+        <div :key="post.id" v-for="post in top3">
+          <p>{{post.body}}</p>
+          <p>{{post.postedDate}}</p>
+          <p>{{post.author.username}}</p>
+          <p>{{post.likes}}</p>
+        </div> 
       </div>
 
   <!-- <RouterView /> -->
